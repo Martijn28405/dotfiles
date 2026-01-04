19ab5ed36ed0d31387cc5a0b785153ebcd90464a
Martijn Verwoert <1049334@hr.nl>
2026-01-04 18:40:31 +0100

    fix trouble plugin

--- nvim/.config/nvim/lua/config/trouble.lua
+++ nvim/.config/nvim/lua/config/trouble.lua
@@ -1,5 +1,18 @@
 local k = vim.keymap.set
-k("n", "<leader>xx", "<cmd>Trouble toggle<cr>", { silent = true })
+
+k("n", "<leader>xx", function()
+  local ok, trouble = pcall(require, "trouble")
+  if ok then
+    trouble.toggle({ mode = "diagnostics" })
+  else
+    vim.cmd("Lazy load trouble.nvim")
+    vim.defer_fn(function()
+      local ok2, trouble2 = pcall(require, "trouble")
+      if ok2 then trouble2.toggle({ mode = "diagnostics" }) end
+    end, 50)
+  end
+end, { silent = true })
+
 k("n", "<leader>xw", "<cmd>Trouble diagnostics toggle<cr>", { silent = true })
 k("n", "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", { silent = true })
 k("n", "<leader>xl", "<cmd>Trouble loclist toggle<cr>", { silent = true })
